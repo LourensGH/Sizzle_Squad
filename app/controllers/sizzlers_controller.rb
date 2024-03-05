e60ac096724d23360e3021b80e21dde26d57d14e
Lourens Kok <lourenskok@gmail.com>
2024-03-05 13:53:17 +0200

    Added sizzler#show view

--- app/controllers/sizzlers_controller.rb
+++ app/controllers/sizzlers_controller.rb
@@ -6,4 +6,24 @@ class SizzlersController < ApplicationController
   def show
     @sizzler = Sizzler.find(params[:id])
   end
+
+  def new
+    @sizzler = Sizzler.new
+  end
+
+  def create
+    @sizzler = Sizzler.new(sizzler_params)
+
+    if @sizzler.save!
+      redirect_to sizzler_path(@sizzler), notice: "Sizzler was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def sizzler_params
+    params.require(:sizzler).permit(:title, :location, :cooking_style, :price, :speciality, :user_id)
+  end
 end
