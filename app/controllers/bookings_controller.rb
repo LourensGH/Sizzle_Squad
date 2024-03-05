0c4686902768deeb85ab14d7b82aa9d24ab086c3
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-05 12:47:21 +0200

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,31 @@
 class BookingsController < ApplicationController
 
-  def create
+  def new
+    @booking = Booking.new
+  end
 
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to root_path, notice: "Booking successfully created."
+    else
+      render :new, status: unprocessable_entity
+    end
+  end
+
+  def show
+    @bookings = Booking.all
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.delete
+    redirect_to root_path
   end
 
   private
 
   def booking_params
-    
+    params.require(:booking).permit(:date, )
   end
 end
