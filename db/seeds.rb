b7a9597eca481498f61c438ffd19eaebcefd3ce9
Chris Roberts <chrisrobertscollective@gmail.com>
2024-03-07 17:04:04 +0200

    stying index

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,21 @@ puts "Creating the Sizzlers"
 # Create 10 Sizzler instances, each one assigned to a random user
 10.times do
   Sizzler.create!(
-    title: Faker::Company.name,
-    location: Faker::Address.city,
+    title: "#{Faker::Name.first_name } #{Faker::Name.last_name }",
+    location: [
+      "1 Adderley St, Cape Town City Centre, Cape Town, 8000, South Africa",
+      "Corner of Darling and Bree St, Cape Town City Centre, Cape Town, 8000, South Africa",
+      "81 Long St, Cape Town City Centre, Cape Town, 8000, South Africa",
+      "10-12 Victoria Rd, Camps Bay, Cape Town, 8005, South Africa",
+      "Corner of Wale St and Burg St, Cape Town City Centre, Cape Town, 8000, South Africa",
+      "15 New Church St, Cape Town City Centre, Cape Town, 8001, South Africa",
+      "V&A Waterfront, 19 Dock Rd, Victoria & Alfred Waterfront, Cape Town, 8001, South Africa",
+      "35 Victoria Rd, Camps Bay, Cape Town, 8005, South Africa",
+      "1 Beach Rd, Mouille Point, Cape Town, 8005, South Africa",
+      "67 Strand St, Cape Town City Centre, Cape Town, 8000, South Africa"
+    ].sample,
     cooking_style: ["Charcoal", "Wood", "Gas", "Hibachi grill"].sample,
-    price: Faker::Commerce.price,
+    price: Faker::Commerce.price(range: 100..500),
     speciality: ["Seafood", "Traditional", "Sosatie", "Potjiekos", "Boerewors", "Braaibroodjies", "Vegetarian"].sample,
     user_id: User.pluck(:id).sample
   )
